1b93d491872747f90b06546cdfff22064f42bb22
pdsuwwz <pdsu.wwz@foxmail.com>
2021-10-26 15:06:06 +0800

    feat: change router mode when deploying

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory
+} from 'vue-router'
 import routes from './routes'
+
+const history = process.env.VITE_ROUTER_MODE === 'hash'
+  ? createWebHashHistory()
+  : createWebHistory()
 
 export default createRouter({
   base: '/',
-  history: createWebHistory(),
+  history,
   routes
 })
