4837cf4ad84f45e48cdfd1c06f085d16557fcc5a
pdsuwwz <pdsu.wwz@foxmail.com>
2021-10-27 17:18:49 +0800

    fix: try fix env on deploy

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import {
 } from 'vue-router'
 import routes from './routes'
 
-const history = import.meta.env.VITE_ROUTER_MODE === 'hash'
+const history = process.env.VITE_ROUTER_MODE === 'hash'
   ? createWebHashHistory()
   : createWebHistory()
 
