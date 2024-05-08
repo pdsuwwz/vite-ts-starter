d3e4bb4990623830e566f42d1a624155da65b995
Wisdom <pdsu.wwz@foxmail.com>
2024-05-08 13:37:32 +0800

    🧱 chore: support stylistic eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,25 +2,27 @@
 // export default antfu()
 
 import globals from 'globals'
-import { defineFlatConfig } from 'eslint-define-config';
+import { defineFlatConfig } from 'eslint-define-config'
 
-import * as parserTypeScript from '@typescript-eslint/parser';
-import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import * as parserTypeScript from '@typescript-eslint/parser'
+import pluginTypeScript from '@typescript-eslint/eslint-plugin'
 
-import * as parserVue from 'vue-eslint-parser';
-import pluginVue from 'eslint-plugin-vue';
-import js from '@eslint/js';
+import * as parserVue from 'vue-eslint-parser'
+import pluginVue from 'eslint-plugin-vue'
+import js from '@eslint/js'
+
+import stylistic from '@stylistic/eslint-plugin'
 
 function renameRules(rules, map) {
   return Object.fromEntries(
     Object.entries(rules).map(([key, value]) => {
       for (const [from, to] of Object.entries(map)) {
-        if (key.startsWith(`${from}/`))
-          return [to + key.slice(from.length), value];
+        if (key.startsWith(`${ from }/`))
+          return [to + key.slice(from.length), value]
       }
-      return [key, value];
+      return [key, value]
     })
-  );
+  )
 }
 
 export default defineFlatConfig([
@@ -33,6 +35,42 @@ export default defineFlatConfig([
       'coverage',
       'src/assets/**'
     ]
+  },
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/no-extra-semi': 'error',
+      '@stylistic/template-curly-spacing': ['error', 'always'],
+      '@stylistic/space-before-blocks': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/object-curly-newline': ['error', {
+        'ObjectExpression': {
+          // 如果对象有属性，则要求换行。空对象则忽略
+          'minProperties': 1,
+          // 保持一致性
+          'consistent': true
+        }
+      }],
+      '@stylistic/object-property-newline': 'error',
+      '@stylistic/key-spacing': ['error', {
+        'beforeColon': false,
+        'afterColon': true
+      }],
+      '@stylistic/type-annotation-spacing': ['error', {
+        'before': true,
+        'after': true,
+        'overrides': {
+          'colon': {
+            'before': false,
+            'after': true
+          }
+        }
+      }],
+      '@stylistic/no-trailing-spaces': ['error']
+    }
   },
   {
     ...js.configs.recommended,
@@ -56,15 +94,27 @@ export default defineFlatConfig([
       sourceType: 'module'
     },
     rules: {
-      'accessor-pairs': ['error', { enforceForClassMembers: true, setWithoutGet: true }],
+      'accessor-pairs': ['error', {
+        enforceForClassMembers: true,
+        setWithoutGet: true
+      }],
       'array-callback-return': 'error',
       'block-scoped-var': 'error',
-      'comma-spacing': ['error', { after: true, before: false }],
+      'comma-spacing': ['error', {
+        after: true,
+        before: false
+      }],
       'constructor-super': 'error',
       'default-case-last': 'error',
-      'dot-notation': ['error', { allowKeywords: true }],
-      'eqeqeq': ['error', 'smart'],
-      'new-cap': ['error', { capIsNew: false, newIsCap: true, properties: true }],
+      'dot-notation': ['error', {
+        allowKeywords: true
+      }],
+      'eqeqeq': ['error', 'always'],
+      'new-cap': ['error', {
+        capIsNew: false,
+        newIsCap: true,
+        properties: true
+      }],
       'no-alert': 'error',
       'no-array-constructor': 'error',
       'no-async-promise-executor': 'error',
@@ -73,7 +123,9 @@ export default defineFlatConfig([
       'no-class-assign': 'error',
       'no-compare-neg-zero': 'error',
       'no-cond-assign': ['error', 'always'],
-      'no-console': ['error', { allow: ['log', 'warn', 'error'] }],
+      'no-console': ['error', {
+        allow: ['log', 'warn', 'error']
+      }],
       'no-const-assign': 'error',
       'no-control-regex': 'error',
       'no-debugger': 'error',
@@ -82,7 +134,9 @@ export default defineFlatConfig([
       'no-dupe-class-members': 'error',
       'no-dupe-keys': 'error',
       'no-duplicate-case': 'error',
-      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-empty': ['error', {
+        allowEmptyCatch: true
+      }],
       'no-empty-character-class': 'error',
       'no-empty-pattern': 'error',
       'no-eval': 'error',
@@ -98,7 +152,10 @@ export default defineFlatConfig([
       'no-invalid-regexp': 'error',
       'no-irregular-whitespace': 'error',
       'no-iterator': 'error',
-      'no-labels': ['error', { allowLoop: false, allowSwitch: false }],
+      'no-labels': ['error', {
+        allowLoop: false,
+        allowSwitch: false
+      }],
       'no-lone-blocks': 'error',
       'no-loss-of-precision': 'error',
       'no-misleading-character-class': 'error',
@@ -112,20 +169,43 @@ export default defineFlatConfig([
       'no-octal-escape': 'error',
       'no-proto': 'error',
       'no-prototype-builtins': 'error',
-      'no-redeclare': ['error', { builtinGlobals: false }],
+      'no-redeclare': ['error', {
+        builtinGlobals: false
+      }],
       'no-regex-spaces': 'error',
       'no-restricted-globals': [
         'error',
-        { message: 'Use `globalThis` instead.', name: 'global' },
-        { message: 'Use `globalThis` instead.', name: 'self' }
+        {
+          message: 'Use `globalThis` instead.',
+          name: 'global'
+        },
+        {
+          message: 'Use `globalThis` instead.',
+          name: 'self'
+        }
       ],
       'no-restricted-properties': [
         'error',
-        { message: 'Use `Object.getPrototypeOf` or `Object.setPrototypeOf` instead.', property: '__proto__' },
-        { message: 'Use `Object.defineProperty` instead.', property: '__defineGetter__' },
-        { message: 'Use `Object.defineProperty` instead.', property: '__defineSetter__' },
-        { message: 'Use `Object.getOwnPropertyDescriptor` instead.', property: '__lookupGetter__' },
-        { message: 'Use `Object.getOwnPropertyDescriptor` instead.', property: '__lookupSetter__' }
+        {
+          message: 'Use `Object.getPrototypeOf` or `Object.setPrototypeOf` instead.',
+          property: '__proto__'
+        },
+        {
+          message: 'Use `Object.defineProperty` instead.',
+          property: '__defineGetter__'
+        },
+        {
+          message: 'Use `Object.defineProperty` instead.',
+          property: '__defineSetter__'
+        },
+        {
+          message: 'Use `Object.getOwnPropertyDescriptor` instead.',
+          property: '__lookupGetter__'
+        },
+        {
+          message: 'Use `Object.getOwnPropertyDescriptor` instead.',
+          property: '__lookupSetter__'
+        }
       ],
       'no-restricted-syntax': [
         'error',
@@ -135,7 +215,9 @@ export default defineFlatConfig([
         'TSEnumDeclaration[const=true]',
         'TSExportAssignment'
       ],
-      'no-self-assign': ['error', { props: true }],
+      'no-self-assign': ['error', {
+        props: true
+      }],
       'no-self-compare': 'error',
       'no-sequences': 'error',
       'no-shadow-restricted-names': 'error',
@@ -147,7 +229,9 @@ export default defineFlatConfig([
       'no-undef-init': 'error',
       'no-unexpected-multiline': 'error',
       'no-unmodified-loop-condition': 'error',
-      'no-unneeded-ternary': ['error', { defaultAssignment: false }],
+      'no-unneeded-ternary': ['error', {
+        defaultAssignment: false
+      }],
       'no-unreachable': 'error',
       'no-unreachable-loop': 'error',
       'no-unsafe-finally': 'error',
@@ -163,7 +247,11 @@ export default defineFlatConfig([
         ignoreRestSiblings: true,
         vars: 'all'
       }],
-      'no-use-before-define': ['error', { classes: false, functions: false, variables: true }],
+      'no-use-before-define': ['error', {
+        classes: false,
+        functions: false,
+        variables: true
+      }],
       'no-useless-backreference': 'error',
       'no-useless-call': 'error',
       'no-useless-catch': 'error',
@@ -173,6 +261,8 @@ export default defineFlatConfig([
       'no-useless-return': 'error',
       'no-var': 'error',
       'no-with': 'error',
+      'space-infix-ops': 'error',
+      'object-curly-spacing': ['error', 'always'],
       'object-shorthand': [
         'error',
         'always',
@@ -181,7 +271,9 @@ export default defineFlatConfig([
           ignoreConstructors: false
         }
       ],
-      'one-var': ['error', { initialized: 'never' }],
+      'one-var': ['error', {
+        initialized: 'never'
+      }],
       'prefer-arrow-callback': [
         'error',
         {
@@ -198,7 +290,9 @@ export default defineFlatConfig([
       ],
       'prefer-exponentiation-operator': 'error',
       'prefer-promise-reject-errors': 'error',
-      'prefer-regex-literals': ['error', { disallowRedundantWrapping: true }],
+      'prefer-regex-literals': ['error', {
+        disallowRedundantWrapping: true
+      }],
       'prefer-rest-params': 'error',
       'prefer-spread': 'error',
       'prefer-template': 'error',
@@ -248,16 +342,25 @@ export default defineFlatConfig([
         }
       }
     },
+    settings: {
+      'import/core-modules': [
+        'uno.css'
+      ]
+    },
     plugins: {
       '@typescript-eslint': pluginTypeScript
     },
     rules: {
       ...renameRules(
         pluginTypeScript.configs['eslint-recommended'].overrides[0].rules,
-        { '@typescript-eslint': 'ts' }
+        {
+          '@typescript-eslint': 'ts'
+        }
       ),
       ...pluginTypeScript.configs.recommended.rules,
-      '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
+      '@typescript-eslint/indent': ['error', 2, {
+        SwitchCase: 1
+      }],
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 1,
@@ -276,7 +379,10 @@ export default defineFlatConfig([
       ],
       '@typescript-eslint/no-empty-function': 0,
       '@typescript-eslint/no-non-null-assertion': 0,
-      '@typescript-eslint/semi': ['error', 'never']
+      '@typescript-eslint/consistent-type-imports': ['error', {
+        fixStyle: 'separate-type-imports',
+        disallowTypeAnnotations: false
+      }]
     }
   },
   {
@@ -334,4 +440,4 @@ export default defineFlatConfig([
       }]
     }
   }
-]);
+])
