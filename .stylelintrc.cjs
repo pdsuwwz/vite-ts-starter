ddccd7933efeeb452af3402cf21c77db6ac91832
Wisdom <pdsu.wwz@foxmail.com>
2025-01-20 17:02:34 +0800

    🦄 chore: upgrade deps

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -30,6 +30,7 @@ module.exports = {
     'scss/double-slash-comment-whitespace-inside': 'always',
     'scss/dollar-variable-pattern': null,
 
+    'declaration-property-value-no-unknown': null,
     'selector-pseudo-class-no-unknown': [
       true,
       {
