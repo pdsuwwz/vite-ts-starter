b3c10246e817173060d35c381d640fef18bf449b
Wisdom <pdsu.wwz@foxmail.com>
2025-03-10 16:44:49 +0800

    💫 chore: upgrade eslint v9.22.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import globals from 'globals'
-import { defineFlatConfig } from 'eslint-define-config'
+import { defineConfig } from 'eslint/config'
+
 
 import * as parserTypeScript from '@typescript-eslint/parser'
 import pluginTypeScript from '@typescript-eslint/eslint-plugin'
@@ -22,7 +23,7 @@ function renameRules(rules, map) {
   )
 }
 
-export default defineFlatConfig([
+export default defineConfig([
   {
     ignores: [
       'public',
@@ -405,10 +406,10 @@ export default defineFlatConfig([
     },
     processor: pluginVue.processors['.vue'],
     rules: {
-      ...pluginVue.configs.base.rules,
-      ...pluginVue.configs['vue3-essential'].rules,
-      ...pluginVue.configs['vue3-strongly-recommended'].rules,
-      ...pluginVue.configs['vue3-recommended'].rules,
+      ...pluginVue.configs['flat/base'].rules,
+      ...pluginVue.configs['flat/essential'].rules,
+      ...pluginVue.configs['flat/recommended'].rules,
+      ...pluginVue.configs['flat/strongly-recommended'].rules,
       'vue/no-v-html': 'off',
       'vue/multi-word-component-names': 0,
       'vue/singleline-html-element-content-newline': 'off',
@@ -423,7 +424,6 @@ export default defineFlatConfig([
         'order': ['script', 'template', 'style']
       }],
       'vue/padding-line-between-blocks': ['error', 'always'],
-      'vue/script-setup-uses-vars': 'error',
       'vue/html-self-closing': ['error', {
         html: {
           void: 'never',
